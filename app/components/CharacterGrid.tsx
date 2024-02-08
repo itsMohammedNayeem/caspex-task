04be1ece6ad97fb8953e994d6f32899867a01e58
itsMohammedNayeem <nayeem.gmit@gmail.com>
2024-02-08 23:32:26 +0530

    UI improved

--- app/components/CharacterGrid.tsx
+++ app/components/CharacterGrid.tsx
@@ -2,17 +2,16 @@ import React, { FC, memo } from "react";
 import { CharacterGridProps } from "../types";
 
 const CharacterGrid: FC<CharacterGridProps> = memo(({ characters }) => {
-    return (
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {characters.map(character => (
-          <div key={character.id} className="p-4 flex flex-col items-center">
-            <img src={character.image} alt={character.name} loading="lazy" className="rounded-full w-24 h-24 object-cover" />
-            <p className="text-white mt-2">{character.name}</p>
-          </div>
-        ))}
-      </div>
-    );
-  });
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+      {characters.map(character => (
+        <div key={character.id} className="p-4 flex flex-col items-center bg-gray-700 rounded-lg">
+          <img src={character.image} alt={character.name} loading="lazy" className="rounded-full w-24 h-24 object-cover" />
+          <p className="text-white mt-2">{character.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+});
 
-  
 export default CharacterGrid;
--- app/components/EpisodeList.tsx
+++ app/components/EpisodeList.tsx
@@ -1,20 +1,17 @@
 'use client';
 import React, { FC, memo, useEffect, useState } from "react";
-import { Character, EpisodeListProps } from "../types"; // Ensure correct import paths
+import { Character, EpisodeListProps } from "../types";
 import CharacterGrid from "./CharacterGrid";
 
 const EpisodeList: FC<EpisodeListProps> = memo(({ episodes }) => {
-  // Set the default selected episode to 1
   const [selectedEpisodeId, setSelectedEpisodeId] = useState<number>(1);
   const [characters, setCharacters] = useState<Character[]>([]);
 
   useEffect(() => {
     const fetchCharacters = async () => {
-      // Find the episode by the selectedEpisodeId
       const episodeData = episodes.find(ep => ep.id === selectedEpisodeId);
       if (!episodeData) return;
 
-      // Fetch characters for the selected episode
       const characterData = await Promise.all(
         episodeData.characters.map((characterUrl: string) =>
           fetch(characterUrl).then(res => res.json())
@@ -28,13 +25,15 @@ const EpisodeList: FC<EpisodeListProps> = memo(({ episodes }) => {
   }, [selectedEpisodeId, episodes]);
 
   return (
-    <div className="flex">
-      <ul className="border-r-2 border-gray-200 max-h-[700px] overflow-y-scroll">
+    <div className="flex justify-center items-start mt-8">
+      <ul className="w-1/4 bg-gray-800 text-white max-h-[700px] overflow-y-scroll">
         {episodes.map((episode) => (
           <li
             key={episode.id}
-            onClick={() => setSelectedEpisodeId(episode.id === selectedEpisodeId ? 1 : episode.id)} // Revert to episode 1 if the same episode is clicked
-            className={`p-2 text-sm cursor-pointer text-white hover:bg-red-200 hover:text-black ${episode.id === selectedEpisodeId ? 'bg-red-500 border' : 'bg-transparent'}`}
+            onClick={() => setSelectedEpisodeId(episode.id === selectedEpisodeId ? 1 : episode.id)}
+            className={`p-4 cursor-pointer hover:bg-red-200 hover:text-black ${
+              episode.id === selectedEpisodeId ? 'bg-red-500 text-black' : ''
+            }`}
           >
             {episode.name}
           </li>
@@ -42,11 +41,11 @@ const EpisodeList: FC<EpisodeListProps> = memo(({ episodes }) => {
       </ul>
 
       <div className="flex-1 p-4">
-        <h2 className="text-white text-md mb-2 text-pretty">
+        <h2 className="text-white text-lg mb-4">
           {characters.length} characters in episode "{episodes.find(ep => ep.id === selectedEpisodeId)?.name}"
         </h2>
         <CharacterGrid characters={characters} />
-        </div>
+      </div>
     </div>
   );
 });
